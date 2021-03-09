23ce731b30da9c838d933df6028390996d119936
Janu <janu.m.sung@gmail.com>
2021-03-08 19:19:49 -0800

    as

--- Arrays/Easy/romanToInt.js
+++ Arrays/Easy/romanToInt.js
@@ -64,4 +64,23 @@ It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 
 let string = "MCMXCIV";
 let string2 = "LVIII";
-const romanToInt = function (string) {};
+
+const romanToInt = function (string) {
+  const romans = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+    IV: 4,
+    IX: 9,
+    XL: 40,
+    XC: 90,
+    CD: 400,
+    CM: 900,
+  };
+
+  let res = 0;
+};
